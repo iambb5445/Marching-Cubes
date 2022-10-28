79f0863d6f47d88d191ab617828d48281d1dc2c0
unknown <bbateni@ucsc.edu>
2022-10-27 21:30:16 -0700

    Add the main marching cubes algorithm, remove templates when not needed, use malloc to allocate array memory

--- Marching Cubes/Main.cpp
+++ Marching Cubes/Main.cpp
@@ -1,73 +1,77 @@
 #include <iostream>
 #include "MarchingCubes.h"
 
-template<int sizeX, int sizeY, int sizeZ>VolumetricData<sizeX, sizeY, sizeZ, int8> getVolumetricDataOfACube();
-template<int sizeX, int sizeY, int sizeZ>VolumetricData<sizeX, sizeY, sizeZ, int8> getVolumetricDataOfARoundEdgeCube();
-template<int sizeX, int sizeY>VolumetricData<sizeX, sizeY, 4, int8> getVolumetricDataOfFlatTerrain();
-template<int sizeX, int sizeY>VolumetricData<sizeX, sizeY, 4, int8> getVolumetricDataOfWavedTerrain();
-
+int get3DIndex(int x, int y, int z, int sizeX, int sizeY, int sizeZ);
+VolumetricData<int8> getVolumetricDataOfACube(int sizeX, int sizeY, int sizeZ);
+VolumetricData<int8> getVolumetricDataOfARoundEdgeCube(int sizeX, int sizeY, int sizeZ);
+VolumetricData<int8> getVolumetricDataOfFlatTerrain(int sizeX, int sizeY);
+VolumetricData<int8> getVolumetricDataOfWavedTerrain(int sizeX, int sizeY);
 
 void main() {
-	auto vData = getVolumetricDataOfACube<10, 10, 10>();
-	auto geo = MarchedGeometry<9, 9, 9>(Vector3D(1, 1, 1), vData);
+	// examples of input data:
+	// auto vData = getVolumetricDataOfACube(5, 5, 5);
+	// auto vData = getVolumetricDataOfARoundEdgeCube(5, 5, 5);
+	// auto vData = getVolumetricDataOfFlatTerrain(5, 5);
+	// auto vData = getVolumetricDataOfWavedTerrain(5, 5);
+	auto vData = getVolumetricDataOfWavedTerrain(5, 5);
+	auto geo = MarchedGeometry(Vector3D(1, 1, 1), vData);
 }
 
-template<int sizeX, int sizeY, int sizeZ>
-VolumetricData<sizeX, sizeY, sizeZ, int8> getVolumetricDataOfACube() {
-	int8 values[sizeX][sizeY][sizeZ];
+int get3DIndex(int x, int y, int z, int sizeX, int sizeY, int sizeZ) {
+	return x * sizeY * sizeZ + y * sizeZ + z;
+}
+
+VolumetricData<int8> getVolumetricDataOfACube(int sizeX, int sizeY, int sizeZ) {
+	int8* values = (int8*)malloc(sizeX * sizeY * sizeZ * sizeof(int8));
 	for (int i = 0; i < sizeX; i++) {
 		for (int j = 0; j < sizeY; j++) {
 			for (int k = 0; k < sizeZ; k++) {
-				values[i][j][k] = -1;
+				values[get3DIndex(i, j, k, sizeX, sizeY, sizeZ)] = -1;
 				if (i == 0 || i == (sizeX - 1) || j == 0 || j == (sizeY - 1) || k == 0 || k == (sizeZ - 1))
-					values[i][j][k] = 0;
+					values[get3DIndex(i, j, k, sizeX, sizeY, sizeZ)] = 0;
 			}
 		}
 	}
-	return VolumetricData<sizeX, sizeY, sizeZ, int8>(values);
+	return VolumetricData<int8>(sizeX, sizeY, sizeZ, values);
 }
 
-template<int sizeX, int sizeY, int sizeZ>
-VolumetricData<sizeX, sizeY, sizeZ, int8> getVolumetricDataOfARoundEdgeCube() {
-	int8 values[sizeX][sizeY][sizeZ];
+VolumetricData<int8> getVolumetricDataOfARoundEdgeCube(int sizeX, int sizeY, int sizeZ) {
+	int8 *values = (int8*)malloc(sizeX * sizeY * sizeZ * sizeof(int8));
 	for (int i = 0; i < sizeX; i++) {
 		for (int j = 0; j < sizeY; j++) {
 			for (int k = 0; k < sizeZ; k++) {
-				values[i][j][k] = -1;
+				values[get3DIndex(i, j, k, sizeX, sizeY, sizeZ)] = -1;
 				if (i == 0 || i == (sizeX - 1) || j == 0 || j == (sizeY - 1) || k == 0 || k == (sizeZ - 1))
-					values[i][j][k] = 1;
+					values[get3DIndex(i, j, k, sizeX, sizeY, sizeZ)] = 1;
 			}
 		}
 	}
-	return VolumetricData<sizeX, sizeY, sizeZ, int8>(values);
+	return VolumetricData<int8>(sizeX, sizeY, sizeZ, values);
 }
 
-template<int sizeX, int sizeY>
-VolumetricData<sizeX, sizeY, 4, int8> getVolumetricDataOfFlatTerrain() {
-
-	int8 values[sizeX][sizeY][4];
+VolumetricData<int8> getVolumetricDataOfFlatTerrain(int sizeX, int sizeY) {
+	int8* values = (int8*)malloc(sizeX * sizeY * 4 * sizeof(int8));
 	for (int i = 0; i < sizeX; i++) {
 		for (int j = 0; j < sizeY; j++) {
-			values[i][j][0] = -1;
-			values[i][j][1] = -1;
-			values[i][j][2] = 0;
-			values[i][j][3] = 1;
+			values[get3DIndex(i, j, 0, sizeX, sizeY, 4)] = -1;
+			values[get3DIndex(i, j, 1, sizeX, sizeY, 4)] = -1;
+			values[get3DIndex(i, j, 2, sizeX, sizeY, 4)] = 0;
+			values[get3DIndex(i, j, 3, sizeX, sizeY, 4)] = 1;
 		}
 	}
-	return VolumetricData<sizeX, sizeY, 4, int8>(values);
+	return VolumetricData<int8>(sizeX, sizeY, 4, values);
 }
 
-template<int sizeX, int sizeY>
-VolumetricData<sizeX, sizeY, 4, int8> getVolumetricDataOfWavedTerrain() {
 
-	int8 values[sizeX][sizeY][4];
+VolumetricData<int8> getVolumetricDataOfWavedTerrain(int sizeX, int sizeY) {
+	int8* values = (int8*)malloc(sizeX * sizeY * 4 * sizeof(int8));
 	for (int i = 0; i < sizeX; i++) {
 		for (int j = 0; j < sizeY; j++) {
-			values[i][j][0] = -1;
-			values[i][j][1] = -1;
-			values[i][j][2] = -((i & 1) + 1);
-			values[i][j][3] = 1;
+			values[get3DIndex(i, j, 0, sizeX, sizeY, 4)] = -1;
+			values[get3DIndex(i, j, 1, sizeX, sizeY, 4)] = -1;
+			values[get3DIndex(i, j, 2, sizeX, sizeY, 4)] = -((i & 1) + 1);
+			values[get3DIndex(i, j, 3, sizeX, sizeY, 4)] = 1;
 		}
 	}
-	return VolumetricData<sizeX, sizeY, 4, int8>(values);
+	return VolumetricData<int8>(sizeX, sizeY, 4, values);
 }
